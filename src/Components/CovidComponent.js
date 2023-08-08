cb1c2b0f15b4127d6824196b2c9c3098b3a08a61
ctokoli <matumasie@gmail.com>
2023-08-08 15:35:59 +0000

    Update CovidComponent.js

--- src/Components/CovidComponent.js
+++ src/Components/CovidComponent.js
@@ -1,21 +1,36 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { getCovidData } from '../Redux/covidSlice';
 
 const CovidComponent = () => {
   const data = useSelector((state) => state.convidData.data);
-  console.log(data);
+  const itemsToRender = data.slice(1, 11);
+  console.log(itemsToRender);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (data.length === 0) {
       dispatch(getCovidData());
     }
-  });
+    console.log('i fire once');
+  }, []);
 
   return (
+
     <>
-      <h2>Hello Covid Component</h2>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the cards content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 };
