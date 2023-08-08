a7cbd3e3cb1ee9665b06b6ec94792ab86b74ade0
ctokoli <matumasie@gmail.com>
2023-08-08 08:03:37 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Provider } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Provider score={store}>
-    <App />
-  </Provider>
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
