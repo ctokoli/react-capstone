ea12e6d97765aee52043ef78beba7bbb2a37703e
ctokoli <matumasie@gmail.com>
2023-08-09 13:10:06 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CovidComponent from './Components/CovidComponent';
 import NavigationLayout from './Components/CovidLayout';
 
 function App() {
   return (
     <>
-      <NavigationLayout />
-      <CovidComponent />
+      <BrowserRouter>
+        <NavigationLayout />
+        <Routes>
+          <Route path="/" element={<CovidComponent />} />
+          <Route path="/:country" />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
