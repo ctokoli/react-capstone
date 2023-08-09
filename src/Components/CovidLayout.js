61546494143cf72b6c220b491401122aaa98b7b9
ctokoli <matumasie@gmail.com>
2023-08-09 10:48:25 +0000

    Add CovidLayout.js

---
+++ src/Components/CovidLayout.js
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const NavigationLayout = () => (
+  <>
+    <Navbar bg="danger" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Form data-bs-theme="light">
+          <Row>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Search by Country"
+              />
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </Navbar>
+  </>
+);
+
+export default NavigationLayout;
