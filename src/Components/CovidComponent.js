08bdcf18a4591472ab67826a89b22b714eb4c220
ctokoli <matumasie@gmail.com>
2023-08-10 09:21:32 +0000

    Update CovidComponent.js

--- src/Components/CovidComponent.js
+++ src/Components/CovidComponent.js
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 import { getCovidData } from '../Redux/covidSlice';
 
 const CovidComponent = () => {
   const data = useSelector((state) => state.convidData.data);
-  const itemsToRender = data.slice(0, 10);
-  console.log(itemsToRender);
+  const itemsToRender = data.slice(0, 16);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (data.length === 0) {
       dispatch(getCovidData());
     }
-    console.log('i fire once');
   });
 
   return (
@@ -21,29 +20,33 @@ const CovidComponent = () => {
     <>
       <Container>
         <div className="grid">
+
           {itemsToRender.map((item) => (
-            <div key={item.Country_text} className="card-item">
-              <div className="active-data">
-                <div className="top">
-                  <h3>
-                    {item.Country_text}
-                  </h3>
-                  <div>
-                    <h4>Total Case</h4>
-                    <h5>333444</h5>
+            <Link to={`/${item.Country_text}`} key={item.Country_text}>
+              <div className="card-item">
+                <div className="active-data">
+                  <div className="top">
+                    <h3>
+                      {item.Country_text}
+                    </h3>
+                    <div>
+                      <h5>Total Cases</h5>
+                      <h6>{item['Total Cases_text']}</h6>
+                    </div>
                   </div>
+                  <h5>Recovered</h5>
+                  <span>{item['Total Recovered_text']}</span>
                 </div>
-                <h5>Total Recorvered</h5>
-                <span>344445</span>
+                <div className="inactive-data">
+                  <h5>
+                    Total Deaths
+                  </h5>
+                  <h6>{item['Total Deaths_text']}</h6>
+                </div>
               </div>
-              <div className="inactive-data">
-                <h4>
-                  Total Death
-                </h4>
-                <h5>{item['Total Deaths_text']}</h5>
-              </div>
-            </div>
+            </Link>
           ))}
+
         </div>
       </Container>
     </>
