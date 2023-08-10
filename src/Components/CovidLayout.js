1a404f96e6e4477b9845d4a8ca1eb25239a25bd7
ctokoli <matumasie@gmail.com>
2023-08-10 17:06:00 +0000

    Update CovidLayout.js

--- src/Components/CovidLayout.js
+++ src/Components/CovidLayout.js
@@ -1,28 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const NavigationLayout = () => (
-  <>
-    <Navbar bg="danger" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand><NavLink to="/">Navbar</NavLink></Navbar.Brand>
-        <Form data-bs-theme="light">
-          <Row>
-            <Col>
-              <Form.Control
-                type="text"
-                placeholder="Search by Country"
-              />
-            </Col>
-          </Row>
-        </Form>
-      </Container>
-    </Navbar>
-  </>
-);
+const NavigationLayout = () => {
+  const country = useParams();
+  console.log(country);
+  return (
+    <>
+      <Navbar bg="danger" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand><NavLink to="/">Navbar</NavLink></Navbar.Brand>
+          <Form data-bs-theme="light">
+            <Row>
+              <Col>
+                <Form.Control
+                  type="text"
+                  placeholder="Search by Country"
+                />
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default NavigationLayout;
