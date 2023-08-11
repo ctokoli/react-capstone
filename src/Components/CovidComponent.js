f751f5be7e5cea34af3338e16a783a830228e5bc
ctokoli <matumasie@gmail.com>
2023-08-11 21:29:40 +0000

    Update CovidComponent.js

--- src/Components/CovidComponent.js
+++ src/Components/CovidComponent.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import { getCovidData } from '../Redux/covidSlice';
+import FilterComponent from './filterComponent';
 
 const CovidComponent = () => {
+  const dispatch = useDispatch();
   const data = useSelector((state) => state.convidData.data);
   console.log(data);
-  const dispatch = useDispatch();
+  const [regionValue, setRegionValue] = useState('');
+
+  const handleButtonClicked = (value) => {
+    console.log(value);
+    setRegionValue(value);
+  };
 
   useEffect(() => {
     if (data.length === 0) {
@@ -16,11 +23,17 @@ const CovidComponent = () => {
     }
   });
 
-  return (
+  // if (data !== undefined) {
+  //   data = data.filter((item) => item.continent === regionValue);
+  // }
 
+  return (
     <>
       <Container>
-        <h2>COVID-19 Statistics Data</h2>
+        <section className="top-page">
+          <h2>COVID-19 Statistics Data</h2>
+          <FilterComponent />
+        </section>
         <div className="grid">
           {data.map((item) => (
             <Link to={`/${item.country}`} key={item.country}>
