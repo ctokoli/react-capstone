5501237e094853e55069dc7a0671516fdabe35ba
ctokoli <matumasie@gmail.com>
2023-08-11 12:36:57 +0000

    Update CovidLayout.js

--- src/Components/CovidLayout.js
+++ src/Components/CovidLayout.js
@@ -1,31 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import leftArrow from '../images/left-arrow-tick.png';
 
 const NavigationLayout = () => (
   <>
-    <Navbar bg="danger">
+    <Navbar bg="back">
       <Container>
         <Navbar.Brand>
           <NavLink to="/">
             <img src={leftArrow} alt="arrow" />
           </NavLink>
-
         </Navbar.Brand>
-        <Form data-bs-theme="light">
-          <Row>
-            <Col>
-              <Form.Control
-                type="text"
-                placeholder="Search by Country"
-              />
-            </Col>
-          </Row>
-        </Form>
       </Container>
     </Navbar>
   </>
