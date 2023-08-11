2d6697ea9b81f3ffe5baa3781cb82933c01708b0
ctokoli <matumasie@gmail.com>
2023-08-11 08:19:08 +0000

    Update DetailComponent.js

--- src/Components/DetailComponent.js
+++ src/Components/DetailComponent.js
@@ -24,10 +24,22 @@ const DetailComponent = () => {
           <div className="details">
             {countryData.map((item) => (
               <div key={item.Country_text}>
-                <div className="country"><h3>{item.Country_text}</h3></div>
-                <div className="total"><h1>{item['Total Cases_text']}</h1></div>
-                <div className="recover"><h1>{item['Total Recovered_text']}</h1></div>
-                <div className="deaths"><h1>{item['Total Deaths_text']}</h1></div>
+                <div className="country detail">
+                  <h4>Country</h4>
+                  <h3>{item.Country_text}</h3>
+                </div>
+                <div className="total detail">
+                  <h4>Total Cases</h4>
+                  <h3>{item['Total Cases_text']}</h3>
+                </div>
+                <div className="recover detail">
+                  <h4>Total Recovered</h4>
+                  <h3>{item['Total Recovered_text']}</h3>
+                </div>
+                <div className="deaths detail">
+                  <h4>Total Deaths</h4>
+                  <h3>{item['Total Deaths_text']}</h3>
+                </div>
               </div>
             ))}
           </div>
