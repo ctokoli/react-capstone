1a1228fa633b6c0badf7c607215ec17cc0aa7418
ctokoli <matumasie@gmail.com>
2023-08-11 16:12:10 +0000

    Update covidSlice.js

--- src/Redux/covidSlice.js
+++ src/Redux/covidSlice.js
@@ -4,11 +4,21 @@ const initialState = {
   data: [],
 };
 
+const url = 'https://covid-193.p.rapidapi.com/statistics';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c3ea3ef7d6mshf9ebf0c1cc5734fp155ae6jsn928f52ac78ea',
+    'X-RapidAPI-Host': 'covid-193.p.rapidapi.com',
+  },
+};
+
 export const getCovidData = createAsyncThunk('get/data', async (rejectWithValue) => {
   try {
-    const response = await fetch('https://covid-19.dataflowkit.com/v1');
-    const data = response.json();
-    return data;
+    const result = await fetch(url, options);
+    const data = await result.json();
+    const itemsToRender = data.response?.slice(100, 140);
+    return itemsToRender;
   } catch (error) {
     console.log(error);
     return rejectWithValue(`there was an arror: ${error}`);
