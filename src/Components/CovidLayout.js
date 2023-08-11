018b28aaf9e11889282edb24ed4477c36da9eefd
ctokoli <matumasie@gmail.com>
2023-08-11 08:19:08 +0000

    Update CovidLayout.js

--- src/Components/CovidLayout.js
+++ src/Components/CovidLayout.js
@@ -1,32 +1,34 @@
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import leftArrow from '../images/left-arrow-tick.png';
 
-const NavigationLayout = () => {
-  const country = useParams();
-  console.log(country);
-  return (
-    <>
-      <Navbar bg="danger" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand><NavLink to="/">Navbar</NavLink></Navbar.Brand>
-          <Form data-bs-theme="light">
-            <Row>
-              <Col>
-                <Form.Control
-                  type="text"
-                  placeholder="Search by Country"
-                />
-              </Col>
-            </Row>
-          </Form>
-        </Container>
-      </Navbar>
-    </>
-  );
-};
+const NavigationLayout = () => (
+  <>
+    <Navbar bg="danger">
+      <Container>
+        <Navbar.Brand>
+          <NavLink to="/">
+            <img src={leftArrow} alt="arrow" />
+          </NavLink>
+
+        </Navbar.Brand>
+        <Form data-bs-theme="light">
+          <Row>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Search by Country"
+              />
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </Navbar>
+  </>
+);
 
 export default NavigationLayout;
