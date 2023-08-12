dedf2cb6023de59b6d6f307fd88267c27ae5a0e0
ctokoli <matumasie@gmail.com>
2023-08-12 22:37:23 +0000

    Update CovidLayout.js

--- src/Components/CovidLayout.js
+++ src/Components/CovidLayout.js
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone, faGear } from '@fortawesome/free-solid-svg-icons';
 import leftArrow from '../images/left-arrow-tick.png';
 
 const NavigationLayout = () => (
@@ -12,6 +15,10 @@ const NavigationLayout = () => (
             <img src={leftArrow} alt="arrow" />
           </NavLink>
         </Navbar.Brand>
+        <Navbar>
+          <FontAwesomeIcon icon={faMicrophone} />
+          <FontAwesomeIcon icon={faGear} />
+        </Navbar>
       </Container>
     </Navbar>
   </>
