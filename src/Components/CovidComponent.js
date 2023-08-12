da827d9ccf66f5f59de46af7bc3ff67ce2c80c62
ctokoli <matumasie@gmail.com>
2023-08-12 17:09:27 +0000

    Update CovidComponent.js

--- src/Components/CovidComponent.js
+++ src/Components/CovidComponent.js
@@ -1,11 +1,11 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import { getCovidData } from '../Redux/covidSlice';
 import FilterComponent from './filterComponent';
 import CardComponent from './CardComponent';
+import LoadingSpinner from './LoadingSpinner';
 
 const CovidComponent = () => {
   const dispatch = useDispatch();
@@ -26,19 +26,24 @@ const CovidComponent = () => {
 
   return (
     <>
-      <Container>
-        <section className="top-page">
-          <h2>COVID-19 Statistics: Filter by Region</h2>
-          <FilterComponent />
-        </section>
-        <div className="grid">
-          {data.map((item) => (
-            <Link to={`/${item.country}`} key={item.country}>
-              <CardComponent item={item} />
-            </Link>
-          ))}
-        </div>
-      </Container>
+      {data.length > 0 ? (
+        <Container>
+          <section className="top-page">
+            <h2>COVID-19 Statistics: Filter by Region</h2>
+            <FilterComponent />
+          </section>
+          <div className="grid">
+            {data.map((item) => (
+              <Link to={`/${item.country}`} key={item.country}>
+                <CardComponent item={item} />
+              </Link>
+            ))}
+          </div>
+        </Container>
+      ) : (
+        <LoadingSpinner />
+      )}
+
     </>
   );
 };
