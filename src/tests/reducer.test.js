43423c850433881c1203d2cfd1295ece1dbf9143
ctokoli <matumasie@gmail.com>
2023-08-12 14:51:46 +0000

    Add reducer.test.js

---
+++ src/tests/reducer.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import reducer, { filterRegions } from '../Redux/covidSlice';
+
+const mockData = [];
+
+describe('Testing the reducer function', () => {
+  it('should show empty state', () => {
+    expect(reducer(undefined, { type: undefined })).toEqual(
+      { data: [], filterData: [] },
+    );
+  });
+
+  it('should show empty state', () => {
+    const previousState = mockData.data;
+    expect(reducer(previousState, filterRegions())).toEqual(
+      { data: [], filterData: [] },
+    );
+  });
+});
