27818d632ea8b5f81b7a60cbb1d34ab890e187dd
ctokoli <matumasie@gmail.com>
2023-08-12 22:37:23 +0000

    Update CardComponent.js

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
 
 const CardComponent = ({ item }) => (
-  <div className="card-item">
-    <div className="active-data">
+  <>
+    <div className="">
       <div className="top">
         <h3>
           {item.country}
         </h3>
       </div>
     </div>
-    <div className="inactive-data">
-      <h5>
+    <div>
+      <h6>
         Total Population
-      </h5>
+      </h6>
       <h6>{item.population}</h6>
     </div>
-  </div>
+  </>
 );
 
 CardComponent.propTypes = {
