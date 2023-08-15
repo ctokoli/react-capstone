3ea10ddd9030e4557f04757cdf46b74c4d47b8ab
ctokoli <matumasie@gmail.com>
2023-08-15 14:16:04 +0000

    Update DetailsCardComponent.js

--- src/Components/DetailsCardComponent.js
+++ src/Components/DetailsCardComponent.js
@@ -2,30 +2,26 @@ import PropTypes from 'prop-types';
 
 const DetailsCard = ({ item }) => (
   <>
-    <div className="country detail">
+    <div className={`country detail ${item.Country_text}`}>
       <h4>Country</h4>
-      <h3>{item.country}</h3>
-    </div>
-    <div className="total detail">
-      <h4>Total Population</h4>
-      <h3>{item.population}</h3>
+      <h3>{item.Country_text}</h3>
     </div>
     <div className="country detail">
       <h4>Total Cases</h4>
-      <h3>{item.cases.total}</h3>
+      <h3>{item['Total Cases_text']}</h3>
     </div>
     <div className="recover detail">
       <h4>Total Recovered</h4>
-      <h3>{item.cases.recovered}</h3>
+      <h3>{item['Total Recovered_text']}</h3>
     </div>
     <div className="deaths detail">
       <h4>Total Deaths</h4>
-      <h3>{item.deaths.total}</h3>
+      <h3>{item['Total Deaths_text']}</h3>
     </div>
   </>
 );
 
 DetailsCard.propTypes = {
-  item: PropTypes.array.isRequired,
+  item: PropTypes.object.isRequired,
 };
 export default DetailsCard;
