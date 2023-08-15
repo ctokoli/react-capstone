8c6980f29ace56f12f85e2e6566147560275d8ec
ctokoli <matumasie@gmail.com>
2023-08-15 08:56:54 +0000

    Update CardComponent.js

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -2,18 +2,18 @@ import PropTypes from 'prop-types';
 
 const CardComponent = ({ item }) => (
   <>
-    <div className="">
+    <div className={item.Country_text}>
       <div className="top">
         <h3>
-          {item.country}
+          {item.Country_text}
         </h3>
       </div>
     </div>
     <div>
       <h6>
-        Total Population
+        Total Recovered
       </h6>
-      <h6>{item.population}</h6>
+      <h6>{item['Total Recovered_text']}</h6>
     </div>
   </>
 );
